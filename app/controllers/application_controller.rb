f6cf933f58c8d0a8f9cc0d370ef5caae053f7ddd
James White <whitejaad@gmail.com>
2023-03-27 10:47:09 -0700

    register page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,7 @@
 class ApplicationController < ActionController::Base
+      def error_message(errors)
+    errors.details.keys.map do |field|
+      errors.full_messages_for(field).first
+    end.join(", ") 
+  end
 end
---
+++ spec/features/register_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+RSpec.describe ' User registration page' do
+  describe "when a user visits '/' page" do
+    it 'shows a for to register, once registered they are redirected to the user show page' do
+      visit "/"
+
+      expect(page).to_not have_content('james@turing.edu')
+
+      click_on('Create New User')
+
+      expect(current_path).to eq("/")
+      expect(page).to have_field(:username)
+      expect(page).to have_field(:email)
+      expect(page).to have_field(:password)
+      expect(page).to have_button('Create New User')
+    end
+  end
+end
