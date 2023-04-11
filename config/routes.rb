6b218b7957d6afe07b0886716862da0be94f91f3
James White <whitejaad@gmail.com>
2023-04-11 11:05:48 -0700

    started working on dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -6,10 +6,12 @@ Rails.application.routes.draw do
   get '/', to: 'welcome#index'
   #post '/', to: 'users#new'
  
-  get 'users/show'
-  get 'users/:id', to: 'users#index'
-  get 'users/:id/discover', to: 'comics#discover'
-  post 'users/:id/discover', to: 'comics#discover'
-  get 'users/:id/comics', to: 'comics#index'
+  #post '/dashboard', to: 'users/show'
+  post '/login', to: 'users#show'
+  post '/users/:id', to: 'users#show'
+  get '/users/:id', to: 'users#index'
+  get '/users/:id/discover', to: 'comics#discover'
+  post '/users/:id/discover', to: 'comics#discover'
+  get '/users/:id/comics', to: 'comics#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
