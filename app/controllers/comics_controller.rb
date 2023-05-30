7ee9bf680d7e0931867321c54e1044ebfead5a2e
James White <whitejaad@gmail.com>
2023-05-30 10:10:07 -0700

    updated to have search funtionality after already searched for comics

--- app/controllers/comics_controller.rb
+++ app/controllers/comics_controller.rb
@@ -6,17 +6,22 @@ def discover
 end
 
   def search
-    @facade = ComicsFacade.new(params[:id], params[:query])
-    if @facade.query_params.blank?
-      flash[:alert] = 'Error: You must provide a query'
-      redirect_to user_discover_comics_path(id: @facade.user_id)
-    else
-      @comics = @facade.comics_keyword
-      render 'comics/index'
-    end
+    @comics = ComicsFacade.new(current_user.id, params[:query]).comics_keyword
+    render 'index'
   end
 
   def show
     @facade = ComicDetailsFacade.new(params[:user_id], params[:id])
   end
 end
+
+#  def search
+#     @facade = ComicsFacade.new(params[:id], params[:query])
+#     if @facade.query_params.blank?
+#       flash[:alert] = 'Error: You must provide a query'
+#       redirect_to user_discover_comics_path(id: @facade.user_id)
+#     else
+#       @comics = @facade.comics_keyword
+#       render 'comics/index'
+#     end
+#   end
